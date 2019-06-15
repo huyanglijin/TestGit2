a2798b2af959b2d051ae05e083415bda5e4a9fa4
huyanglijin <huyanglijin@163.com>
2019-06-15 18:01:45 +0800

    aaaa

---
+++ src/main/java/com/luoluo/lalalala/LalalalaApplication.java
@@ -0,0 +1,13 @@
+package com.luoluo.lalalala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalalalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LalalalaApplication.class, args);
+	}
+
+}
